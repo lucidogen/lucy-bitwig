ee5e0d05d2868ebe9f55f3e63c9b4d2bcd3cc270
Gaspard Bucher <gaspard@lucidogen.io>
2016-02-22 14:14:13 +0100

    Working 8 macro control on selected track.

--- Code/Code.control.js
+++ Code/Code.control.js
@@ -1,7 +1,66 @@
+/* Lucidogen's version of Bitwig Studio control with Livid Code
+ * ------------------------------------------------------------
+ *
+ * Goals
+ * -----
+ *  Our goal with this script is to enable "eyes-less" control of effects
+ *  and plugins.
+ *
+ * How it works
+ * ------------
+ *
+ *  8 first macro on track
+ *  ----------------------
+ *
+ *  When selecting a track in Bitwig Studio, the first 8 Macro settings are
+ *  mapped to the first 8 encoders on the Code:
+ *      +---------------------+
+ *      |  O x x x x . . . .  |
+ *      |  O x x x x . . . .  |
+ *      |  O . . . . . . . .  |
+ *      |  O . . . . . . . .  |
+ *      |  O O O O O O O O O  |
+ *      +---------------------+
+ *  
+ * We need to set Livid Code to defaults,
+ * enc speed A = 1x
+ * enc speed B = 1/4x
+ * top left button momentary enc speed
+ */
 loadAPI (1)
+
+var VERSION = '1.0'
 
 // Number of controls watched on selected track
 var STRACK_CONTROLS_COUNT = 8
+var RESOLUTION = 128
+var MACROS   = []
+// Bitwig macro layout
+var BW_MACROS =
+[  1,  2,  3,  4,   17, 18, 19, 20
+,  5,  6,  7,  8,   21, 22, 23, 24
+
+,  9, 10, 11, 12,   25, 26, 27, 28
+, 13, 14, 15, 16,   29, 30, 31, 32
+]
+// We use default mapping for simplification
+var LIVID_CODE =
+[  1,  5,  9, 13,   17, 21, 25, 29
+,  2,  6, 10, 14,   18, 22, 26, 30
+
+,  3,  7, 11, 15,   19, 23, 27, 31
+,  4,  8, 12, 16,   20, 24, 28, 32
+]
+
+var CTRL_TO_MAC = []
+var MAC_TO_CTRL = []
+for ( var i = 0; i < BW_MACROS.length; ++i )
+{ var ctrl = LIVID_CODE [ i ]
+  var mac  = BW_MACROS  [ i ]
+  CTRL_TO_MAC [ ctrl ] = mac
+  MAC_TO_CTRL [ mac  ] = ctrl
+}
+  
 
 // set properties
 host.defineController
@@ -18,40 +77,45 @@ function init ()
   host.getMidiOutPort ( 0 )
 
   // Selected track
-  strack =
+  var strack =
   host.createArrangerCursorTrack ( 2, 0 )
 
   // Devices of selected track
-  sdevices =
+  var sdevices =
   strack.createDeviceBank ( STRACK_CONTROLS_COUNT )
 
-  sdevices
-  .getDevice ( 0 )
-  .getMacro  ( 0 )
-  .getAmount ()
-  .addValueObserver
-  ( 128, function ( value )
-    { println( 'MACRO1: ' + value )
-      // channel 1, cc 1, value
-      midiOut.sendMidi ( 0xB0, 1, value )
-    }
-  )
+  // First device in selected track
+  var firstDevice = sdevices.getDevice ( 0 )
 
-  sdevices
-  .getDevice ( 0 )
-  .getMacro  ( 1 )
-  .getAmount ()
-  .addValueObserver
-  ( 128, function ( value )
-    { println( 'MACRO2: ' + value )
-      midiOut.sendMidi ( 0xB0, 5, value )
-    }
-  )
+  function setupMacro ( mac )
+  { var macro = firstDevice.getMacro ( mac - 1 ).getAmount ()
+    var ctrl  = MAC_TO_CTRL [ mac ]
+    macro.addValueObserver
+    ( RESOLUTION, function ( value )
+      { // channel 1, cc, value
+        midiOut.sendMidi ( 0xB0, ctrl, value )
+      }
+    )
+    macro.setIndication ( true )
+    MACROS [ mac ] = macro
+  }
+
+  for ( var i = 1; i <= STRACK_CONTROLS_COUNT; ++i )
+  { setupMacro ( i )
+  }
+
+  println ( 'Lucidogen Code v ' + VERSION + ' setup completed.')
 }
 
 function onMidi ( status, data1, data2 )
 {
   if ( isChannelController ( status ) )
-  { // ...
+  { var mac = CTRL_TO_MAC [ data1 ]
+    if ( mac )
+    { var m = MACROS [ mac ]
+      if ( m )
+      { m.set ( data2, RESOLUTION )
+      }
+    }
   }
 }
